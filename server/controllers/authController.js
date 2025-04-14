7144f71c176cb35b81ccecb87679fd7481b88be1
Ritika Sah <ritikasah731@gmail.com>
2025-04-14 09:56:47 +0530

    Login and Log out

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -10,7 +10,7 @@ export const register = async (req,res)=>{
     try {
         const existingUser = await userModel.findOne({email})
         if(existingUser){
-            return res.json({success:false, message:'Usetr already exists'})
+            return res.json({success:false, message:'User already exists'})
         }
         const hashedPassword = await bcrypt.hash(password, 10)
         const user = new userModel({name, email, password: hashedPassword})
@@ -23,9 +23,64 @@ export const register = async (req,res)=>{
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none': 'strict',
             maxAge: 7 * 60 * 60 * 60 * 1000
+           
         })
  
+        return res.json({success:true}) 
+
     } catch (error) {
        res.json({success:false, message:error.message}) 
+    }
+}
+
+
+export const login = async()=>{
+    const{email,password}=req.body;
+
+    if(!email || !password){
+        return res.json({success:false, message:'Email and password are required'})
+    }
+
+    try {
+        const user = await userModel.findOne({email});
+        if(!user){
+            return res.json({success:false, message:'Invalid email'})
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.json({success:false, message:'Invalid password'})
+        }
+        
+        const token=jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:'7d'});
+
+        res.cookie('token', token ,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none': 'strict',
+            maxAge: 7 * 60 * 60 * 60 * 1000
+        })
+
+        return res.json({success:true}) 
+
+    } catch (error) {
+        res.json({success:false, message:error.message}) 
+    }
+}
+
+
+export const logout = async(req,res)=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none': 'strict',
+            maxAge: 7 * 60 * 60 * 60 * 1000
+        })
+        
+        return res.json({success:true, meesagea: "Logged out"})
+
+    } catch (error) {
+         return res.json({success:false, message:error.message}) 
     }
 }
