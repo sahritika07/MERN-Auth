67935ef68a2d11feb87de391c9afb971124e579d
Ritika Sah <ritikasah731@gmail.com>
2025-04-14 13:53:07 +0530

    Changes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 export const register = async (req,res)=>{
     const {name,email,password} = req.body;
@@ -22,10 +23,20 @@ export const register = async (req,res)=>{
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none': 'strict',
-            maxAge: 7 * 60 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000
            
-        })
+        });
+
+        // SENDING WELCOME EMAIL
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: "Welcome to Developer's World",
+            text:`Welcome to Developer World Website . Your account has been created with email id: ${email}`
+        }
  
+        await transporter.sendMail(mailOptions);
+        console.log(mailOptions)
         return res.json({success:true}) 
 
     } catch (error) {
