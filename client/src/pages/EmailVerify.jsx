9532b51798944b62c6f69a4deb211249a87b5230
Ritika Sah <ritikasah731@gmail.com>
2025-04-18 23:38:56 +0530

    Creating otp forms

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
 
 const EmailVerify = () => {
+  const inputRefs = React.useRef([])
+  const handleInput = (e, index)=> {
+     if(e.target.value.length > 0 && index < inputRefs.current.length - 1){
+      inputRefs.current[index+1].focus()
+     }
+  }
+  const handleKeyDown = (e, index) =>{
+    if(e.key === 'Backspace' && e.target.value === '' && index > 0){
+      inputRefs.current[index-1].focus()
+    }
+  }
   return (
-    <div>
-      <h1>Email Verify</h1>
+    <div className='flex items-center min-h-screen justify-center  bg-gradient-to-br from-blue-200 to-purple-400'>
+       <img onClick={() => navigate('/')} src={assets.logo} className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+       <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
+        <p className='text-center mb-6 text-indigo-300'>Enter the 6 digit code sent to your email id</p>
+        <div className='flex justify-between mb-8'>
+            {Array(6).fill(0).map((_,index)=>(
+                <input className='w-12 h-12 bg-[#333A5C] text-center text-white text-xl rounded-md' 
+                ref={e => inputRefs.current[index] = e}
+                onInput={(e) => handleInput(e,index)}
+                onKeyDown={(e) => handleKeyDown(e,index)}
+                type="text" max-Length='1' key={index} required/>
+            ))}
+        </div>
+        <button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 rounded-full'>Verify Email</button>
+       </form>
     </div>
   )
 }
