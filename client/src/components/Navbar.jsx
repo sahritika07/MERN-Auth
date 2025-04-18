9f796e056397e4f5ab86122ec9951cee2462429a
Ritika Sah <ritikasah731@gmail.com>
2025-04-18 21:31:09 +0530

    Integrating frontend with backend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { assets } from '../assets/assets.js'
 import {useNavigate} from 'react-router-dom'
+import { AppContext } from '../context/AppContext.jsx'
 
 const Navbar = () => {
   const navigate = useNavigate()
+  const {userData,backendUrl,SetUserData,setIsLoggedin} = useContext(AppContext)
   return (
     <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
       <img src={assets.logo} alt="" className='w-20 sm:w-28 ' />
-      <button onClick={()=> navigate('/login')}  className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon}/> </button>
+      {userData ? 
+      <div className='w-8 h-8 flex justify-center items-center rounded-full relative text-white bg-black group'> {userData.name[0].toUpperCase()}
+      <div className='absolute hidden group-hover:block top-0 right-0 z-10  text-blacl rounded pt-10'> 
+        <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
+          <li className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>
+          <li  className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
+        </ul>
+      </div>
+       </div> : 
+      <button onClick={()=> navigate('/login')}  className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>Login <img src={assets.arrow_icon}/> </button>}
+      
     </div>
   )
 }
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState , useEffect } from "react";
 import axios from 'axios'
 
 export const AppContext = createContext()
@@ -7,15 +7,33 @@ export const AppContextProvider = (props) =>{
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
+    const getAuthState = async() =>{
+        try {
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
+            if(data.success) {
+                setIsLoggedin(true)
+                getUserData()
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const getUserData = async()=>{
         try {
             const {data} = await axios.get(backendUrl + '/api/user/data')
             data.success ? setUserData(data.userData): toast.error(data.message)
         } catch (error) {
-            toast.error(data.message)
+            toast.error(error.message)
         }
     }
+
+    useEffect(() => {
+      getAuthState();
+    }, [])
+    
+
     const value = {
       backendUrl,
       isLoggedin,setIsLoggedin,
