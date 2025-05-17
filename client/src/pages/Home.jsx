adf58b6e10e0c1412f00e5398bc2fc424d38a7dd
Ritika Sah <ritikasah731@gmail.com>
2025-05-17 22:10:56 +0530

    Changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
-// import Particles from './Particles';
+
 
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen  bg-cover  bg-center'>
+    <div className='relative flex flex-col items-center justify-center min-h-screen bg-cover bg-center overflow-hidden'>
+  
 
-
-      {/* <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-        <Particles
-          particleColors={['#ffffff', '#ffffff']}
-          particleCount={200}
-          particleSpread={10}
-          speed={0.1}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
-          alphaParticles={false}
-          disableRotation={false}
-        />
-      </div> */}
+      {/* Content */}
       <Navbar />
       <Header />
     </div>
